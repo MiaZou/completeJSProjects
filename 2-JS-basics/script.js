4aa104bf795df851c5cc565dd6728b228c697619
MiaaaaaaZ <mirongzou@gmail.com>
2020-02-29 17:10:51 -0800

    bills/price

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -68,7 +68,31 @@ if (averageJohn > averageMike && averageJohn > averageMary) {
 }
 
 
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
 
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+GOOD LUCK 😀
+*/
+
+let bill = [124, 48, 268];
+
+function totalPrice(bill) {
+  if (bill < 50) return bill * 1.2;
+  if (50 <= bill < 200) return bill * 1.15;
+  if (bill >= 200) return bill * 1.1;
+}
+
+let total = [totalPrice(bill[0]), totalPrice(bill[1]), totalPrice(bill[2])];
+
+console.log(total);
 
 
 
