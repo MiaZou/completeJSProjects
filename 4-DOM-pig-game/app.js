51ec90388c4a2584643b3539d4d01a21ac81d82c
MiaaaaaaZ <mirongzou@gmail.com>
2020-03-01 17:07:43 -0800

    dice, score

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -9,8 +9,35 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer;
+var scores, roundScores, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
