5e7b8771bad428f8bb132f29a4f53d425b2ce0f9
MiaaaaaaZ <mirongzou@gmail.com>
2020-03-01 17:28:36 -0800

    roll the dice

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -9,16 +9,14 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer, dice;
+var scores, roundScores, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 var x = document.querySelector('#score-0').textContent;
@@ -26,7 +24,21 @@ console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  let dice = Math.floor(Math.random() * 6) + 1;
+
+  let diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  document.querySelector('#current-' + activePlayer).textContent = dice;
+  
+});
 
 
 
