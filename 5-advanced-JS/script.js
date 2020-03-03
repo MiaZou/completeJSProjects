3672a9aee3b2beae3e330fab1a0c7649a6f58207
MiaaaaaaZ <mirongzou@gmail.com>
2020-03-02 17:20:06 -0800

    primitives - object

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -29,3 +29,66 @@ mark.calcAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+
+
+// Object.create
+var personProto = {
+  calcuAge: function() {
+    console.log(2020 - this.yearOfBirth);
+  }
+}
+
+var jason = Object.create(personProto);
+jason.name = 'Jason';
+jason.yearOfBirth = 1990;
+jason.job = 'teacher';
+
+var jenny = Object.create(personProto, {
+  name: { value: 'Jenny' },
+  yearOfBirth: { value: 1969 },
+  job: { value: 'designer' }
+});
+
+// Primitives vs objects
+
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+var obj1 = {
+  name: 'John',
+  age: 26
+};
+
+var obj2 = obj1;
+obj1.age =30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
