3551a03195486364732be2c9b5151dacbddac55c
MiaaaaaaZ <mirongzou@gmail.com>
2020-03-02 22:02:53 -0800

    functions returning functions

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -51,6 +51,7 @@ var jenny = Object.create(personProto, {
 
 // Primitives vs objects
 
+// Objects
 var a = 23;
 var b = a;
 a = 46;
@@ -62,18 +63,84 @@ var obj1 = {
   age: 26
 };
 
-var obj2 = obj1;
+var obj2 = obj1; // we did not create new object, but create new reference which points to a new reference.
 obj1.age =30;
 console.log(obj1.age);
 console.log(obj2.age);
 
+// Functions
+var age = 27;
+var obj = {
+  name: 'Jonas',
+  city: 'Lisbon'
+};
+
+function change(a, b) {
+  a = 30;
+  b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
 
 
+var years = [1990, 1965, 1937, 2005, 1998];
 
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i=0; i<arr.length;i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
 
+function calculAge(el) {
+  return 2020 - el;
+}
 
+function isFullAge(el) {
+  return el >= 18;
+}
 
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - (0.67 * el));
+  } else {
+    return -1;
+  }
+}
 
+var ages = arrayCalc(years, calculAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
+
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name + ', can you please explain what UX design is?');
+    }
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log(name + ', what subject do you teach?');
+    }
+  } else {
+    return function(name) {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+
+var teacherQ = interviewQuestion('teacher');
+var designerQ = interviewQuestion('designer');
+
+teacherQ('John');
+designerQ('John');
 
 
 
