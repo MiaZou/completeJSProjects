abf4fa96a14a341c3cdaf9dfe68c82fa21f53ee4
MiaaaaaaZ <mirongzou@gmail.com>
2020-03-04 19:36:49 -0800

    data structure

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -7,6 +7,53 @@ var budgetController = (function() {
     this.value = value;
   }
 
+  let Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  let data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      let newItem, ID;
+
+      // create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // create new item based on 'inc' or 'exp' type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push it into our data structure
+      data.allItems[type].push(newItem);
+
+      // Return the new element:
+      return newItem;
+    },
+
+    testing: function() {
+      console.log(data);
+    }
+  };
+
 }) ();
 
 
@@ -29,6 +76,15 @@ var UIController = (function(){
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+
+    addListItem: function(obj, type) {
+
+      // Create HTML string with placeholder text
+
+      // Replace the placeholder text with some actual data
+
+      // Insert the HTML into the DOM
     },
 
     getDOMstrings: function() {
@@ -60,8 +116,11 @@ var controller = (function(budgetCtrl, UICtrl){
 
 
   let ctrlAddItem = function() {
-    let input = UICtrl.getInput();
-    console.log(input);
+    let input, newItem;
+
+    input = UICtrl.getInput();
+
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
   }
 
   return {
