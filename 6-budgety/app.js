90d28d89db1a03aea2577d1fdc23857c362f119a
MiaaaaaaZ <mirongzou@gmail.com>
2020-03-04 20:48:18 -0800

    nan/float

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -76,7 +76,7 @@ var UIController = (function(){
       return {
         type: document.querySelector(DOMstrings.inputType).value, // inc or exp
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       };
     },
 
@@ -141,18 +141,35 @@ var controller = (function(budgetCtrl, UICtrl){
     });
   };
 
+  let updateBudget = function() {
+    // 1. calculate the budget
+
+    // 2. return the budget
+
+    // 3. Display the budget on the UI
+  };
+
 
   let ctrlAddItem = function() {
     let input, newItem;
 
+    // 1. Get the field input data
     input = UICtrl.getInput();
 
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+    if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+      // 2. Add the item to the budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-    UICtrl.addListItem(newItem, input.type);
+      // 3. Add the item to the UI
+      UICtrl.addListItem(newItem, input.type);
 
-    UICtrl.clearFields();
-  }
+      // 4. Clear the fields
+      UICtrl.clearFields();
+
+      // 5. Calculate and update budget
+      updateBudget();
+    }
+  };
 
   return {
     init: function() {
